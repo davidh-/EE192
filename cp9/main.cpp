70cdceec6a7a3dee028a11c4181df541b655b5d3
David Dominguez Hooper <davidhooper4@berkeley.edu>
2017-03-22 17:31:02 -0700

    checkpoint 9 ready for demo

--- cp9/main.cpp
+++ cp9/main.cpp
@@ -1,120 +1,90 @@
 #include "mbed.h"
-#include "TSL1401.h"
+#include "telemetry.h"
+#include "MODSERIAL.h"
+// Telemetry:
+MODSERIAL telemetry_serial(PTC15, PTC14); 
+
+telemetry::MbedHal telemetry_hal(telemetry_serial);
+telemetry::Telemetry telemetry_obj(telemetry_hal);
+
+telemetry::Numeric<uint32_t> tele_time_ms(telemetry_obj, "time", "Time", "ms", 0);
+telemetry::NumericArray<uint16_t, 128> tele_linescan(telemetry_obj, "linescan", "Linescan", "ADC", 0);
+telemetry::Numeric<float> tele_servo_pwm(telemetry_obj, "servo", "Servo PWM", "PWM (us)", 0);
+telemetry::Numeric<float> tele_motor_pwm(telemetry_obj, "motor", "Motor PWM", "%DC", 0);
 
 
-#define TAOS_SI_HIGH    TAOS_SI = 1
-#define TAOS_SI_LOW     TAOS_SI = 0
-#define TAOS_CLK_HIGH   TAOS_CLK = 1
-#define TAOS_CLK_LOW    TAOS_CLK = 0
-
+// Line Camera Pins
 DigitalOut TAOS_SI(D0);
 DigitalOut TAOS_CLK(D1);
-
 AnalogIn A_IN(A5);
 
-
-//Interrupts
+// Interrupts
 //InterruptIn hall(D2);
-Ticker servo_tick;
-//Ticker camera_tick;
-//Timer t;
-Ticker print;
+
+// Tickers
+Ticker camTick;
+Ticker servoTick;
+Ticker telemTick;
 
 //Digital pin to read from Hall Sensor
 PwmOut motor(D13);
 DigitalOut brakeEN(D12);
 
+// Servo Pin
 PwmOut servo(D9);
-TSL1401 cam(D0, D1, A5);
-Serial bt(PTC15, PTC14);
-Serial pc(USBTX, USBRX);
 
-//DAVID
-//float l_end = 600;
-//float r_end = 1500;
-float l_end = 1090;
-//1480 center
-float r_end = 1850;
+// Telemetry Pins
+//Serial bt(PTC15, PTC14);
+//Serial pc(USBTX, USBRX);
 
-float steps = r_end - l_end;
+// Servo endpoints
+float l_end = 1020;
+//1440 center
+float r_end = 1750;
+
+//// Servo endpoints
+//float l_end = 1120;
+////1440 center
+//float r_end = 1850;
+
+// Servo steps
 float divi = 128.0;
-//20% - left = 1000
-//28% - center
-//36% - right = 1800
+float steps = r_end - l_end;
 float step = steps/divi;
-float throttle = 0.0;
+
+// Servo position
 float pw = 0.0;
 float max = 0.0;
 int max_i = 0;
-
 float pos = 0.0;
 
+// Motor PWM = 0.07 = 7%
+float motor_pwm = 0.07f;
+
+// Line cam vars
 int PIXELS = 128;
+unsigned short data[128];
 float t_SI = 0.00000002; //20ns
 float t_hold = 0.000000005; //5ns
 float t_w = 0.00000005; //50ns
-//    float t_qt = 0.1; //100 ms
 float t_qt = 0.00003375; //33.75 us
-//    float t_qt = 0.00002; //20us
-unsigned short data[128];
 
-void control() {
-    char c = ' ';
-    if (pc.readable()) {
-        c = pc.getc();
-    }
-    if (bt.readable()) {
-        c = bt.getc();
-    }
-    if (c == 'q') {
-        t_qt += 0.000001;
-    }
-    else if (c == 'w') {
-        t_qt -= 0.000001;
-    }
-    else if (c == 's') {
-        t_SI += 0.000000001;
-    }
-    else if (c == 'd') {
-        t_SI -= 0.000000001;
-    }
-    else if (c == 'h') {
-        t_hold += 0.0000000001;
-    }
-    else if (c == 'j') {
-        t_hold -= 0.0000000001;
-    }
-    else if (c == 't') {
-        t_w += 0.000000001;
-    }
-    else if (c == 'y') {
-        t_w -= 0.000000001;
-    }
-    else if (c == 'r') {
-        t_SI = 0.00000002; //20ns
-        t_hold = 0.000000005; //5ns
-        t_w = 0.00000005; //50ns
-        t_qt = 0.00003375; //33.75 us
-    }
-    c = ' ';
-}
+
 
 void print_serial() {
-//    pc.printf("print: ");
-//    bt.printf("print: ");
+    telemetry_obj.do_io();
+//    pc.printf("\n");
+//    bt.printf("\n");
 //    for(int i = 0; i < PIXELS; i++) {
-//        pc.printf("%8.5u ", (data[i]));
-//        bt.printf("%8.5u ", (data[i]));
+//        int v = (data[i]/(65535*.5));
+//        pc.printf("%i", v);
+//        bt.printf("%i", v);
 //    }
 //    pc.printf("\nmax: %i, pos: %f, l_end: %f, r_end: %f\r\n", max_i, pos, l_end, r_end);
 //    bt.printf("\nmax: %i, pos: %f, l_end: %f, r_end: %f\r\n", max_i, pos, l_end, r_end);
-//    control();
-//    pc.printf("t_SI: %E, t_hold: %E, t_w: %E, t_qt: %E\r\n", t_SI, t_hold, t_w, t_qt);
-//    bt.printf("t_SI: %E, t_hold: %E, t_w: %E, t_qt: %E\r\n", t_SI, t_hold, t_w, t_qt);
+
 }
 void updateServoFromCam() {
-
-
     TAOS_SI = 0;
     TAOS_CLK = 0;
 
@@ -132,11 +102,8 @@ void updateServoFromCam() {
         TAOS_CLK = 1;
         wait(t_w);
         data[i] = A_IN.read_u16();
-//        int v = (data[i]/65535);
-//        pc.printf("%i", v);
-//        bt.printf("%i", v);
-        //calc max:
-        if (data[i] > max) {
+        tele_linescan[i] = data[i];
+        if (data[i] > max) { //calc max
             max = data[i];
             max_i = i;
         }
@@ -145,66 +112,56 @@ void updateServoFromCam() {
     }
     max = 0;
     wait(t_qt);
-//        cycles += 1;
-//        break;
-
     pos = r_end - (step * max_i);
     pw = pos * (0.000001);
+}
+void updateMotorPWM() {
+    tele_motor_pwm = motor_pwm*100;    
 }
 
 void writePWMServo() {
     if (max != 0) {
+        tele_servo_pwm = pw*1000000;
         servo.pulsewidth(pw);
     }
 }
 
-////OPEN LOOP
-int main() {
 
-    //Set servo PWM period
-    servo.period_ms(5);
-    servo_tick.attach(&writePWMServo, 0.010);
-    print.attach(&print_serial, 0.5);
-    //camera_tick.attach(&updateServoFromCam, 0.005);
+void bluetoothsetup() {
+    telemetry_serial.baud(9600);
+    wait(1.1);
+    telemetry_serial.printf("$$$");
+    wait(1.1);
+    telemetry_serial.printf("SU,38");
+    wait(1.1);
+    telemetry_serial.printf("R,0");
+    wait(1.1);
+}
+//OPEN LOOP
+int main() {
+    telemetry_serial.baud(38400);
+    
+    tele_motor_pwm.set_limits(0.0, 20.0); // lower bound, upper bound
+    tele_servo_pwm.set_limits(0.0, 2000.0); // lower bound, upper bound
+    telemetry_obj.transmit_header();
+    
+    servo.period_ms(3); //Set servo PWM period = 3ms
 
     //Set BrakeEN to HIGH
     brakeEN.write(1);
 
     //set pwm
-    motor.period_us(50);      // 20 kHz frequency
-    motor.write(0.10f);
+    motor.period_us(50); // 20 kHz frequency
+    motor.write(motor_pwm); //12% duty cycle
+    
+    servoTick.attach(&writePWMServo, 0.003); //333Hz = 3ms = 0.003s
 
-    while(1) {
+    while (1) {
         updateServoFromCam();
+        updateMotorPWM();
+        tele_time_ms = telemetry_hal.get_time_ms();
+//        telemTick.attach(&print_serial, 0.5);
+        telemetry_obj.do_io();
     }
 }
 
-//void updateServoFromCam() {
-//    int *data;
-//    data = cam.Capture(0, 127);
-//    int left = 0;
-//    bool left_set = false;
-//    int right = 0;
-//    for (int i = 0; i < 125; i++) {
-//        int v = (data[i]/65535);
-//        printf("%i\n\r", v);
-//        if (v == 1 and !left_set) {
-//            left = i;
-//            left_set = true;
-//        }
-//        if (v == 1) {
-//            right = i;
-//        }
-//    }
-//    int max = (left + right)/2;
-//    //printf(" max: %i, ", max);
-//    float pos = 2000 - (step * max);
-//    float pw = pos * (0.000001);
-//    printf("pos: %f, ", pos);
-//}
-//
-//void writePWMServo() {
-//    if (max != 0) {
-//        servo.pulsewidth(pw);
-//    }
-//}
--- cp9/mbed_config.h
+++ cp9/mbed_config.h
@@ -5,9 +5,24 @@
 #define __MBED_CONFIG_DATA__
 
 // Configuration parameters
-#define MBED_CONF_PLATFORM_STDIO_BAUD_RATE          9600 // set by library:platform
+#define MBED_CONF_LWIP_TCP_SERVER_MAX               4    // set by library:lwip
+#define MBED_CONF_LWIP_ADDR_TIMEOUT                 5    // set by library:lwip
 #define MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE 9600 // set by library:platform
+#define MBED_CONF_LWIP_IPV4_ENABLED                 1    // set by library:lwip
+#define MBED_CONF_LWIP_TCP_SOCKET_MAX               4    // set by library:lwip
+#define MBED_CONF_EVENTS_PRESENT                    1    // set by library:events
+#define MBED_CONF_RTOS_PRESENT                      1    // set by library:rtos
+#define CFSTORE_STORAGE_DISABLE                     0    // set by library:configuration-store
 #define MBED_CONF_PLATFORM_STDIO_FLUSH_AT_EXIT      1    // set by library:platform
+#define MBED_CONF_LWIP_UDP_SOCKET_MAX               4    // set by library:lwip
+#define MBED_CONF_NSAPI_PRESENT                     1    // set by library:nsapi
+#define MBED_CONF_FILESYSTEM_PRESENT                1    // set by library:filesystem
+#define MBED_CONF_LWIP_IP_VER_PREF                  4    // set by library:lwip
 #define MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES   0    // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_BAUD_RATE          9600 // set by library:platform
+#define MBED_CONF_LWIP_SOCKET_MAX                   4    // set by library:lwip
+#define MBED_CONF_LWIP_IPV6_ENABLED                 0    // set by library:lwip
+// Macros
+#define UNITY_INCLUDE_CONFIG_H                           // defined by library:utest
 
 #endif
